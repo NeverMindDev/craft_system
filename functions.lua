2bb819e0c0becac04e62f25a4cef23c13b9bf23c
NeverMindDev <160769327+NeverMindDev@users.noreply.github.com>
2024-03-12 02:49:09 +0100

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -4,12 +4,12 @@ Config.Locale = "en"            -- hu, en
 
 Config.Title = true             -- if your notify support this extra message
 
-Config.Webhook = 'https://discord.com/api/webhooks/1214515476290338836/ktTxYA9LH5ts299nx--KgC2ckzPc85lar9nhzVybKJmCQHthKpIC0u24ooD_BmPyIAHZ'             -- discord Webhook
+Config.Webhook = ''             -- discord Webhook
 
 Config.Command = "resetcraft"   -- command to set all crafts 0
 Config.UseCommand = {           -- groups that can use the command.
     ["admin"] = true,
-    ["tulaj"] = true
+    ["owner"] = true
 }
 
 
